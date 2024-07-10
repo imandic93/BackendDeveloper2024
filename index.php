8111ccaf8d04500b23d763f7f559a423cb10884c
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-10 19:38:49 +0200

    Nizovi i prva vjezba

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
-$ime = 'Ivan';
-$prezime = 'Mandic';
-$imePrezime = $ime . ' ' . $prezime;
+$array = [1, 2, 3];
+$array2 = [
+    'name' => 'Ivan',
+    'age' => 31,
+    'skills' => ['PHP', 'MySQL', 'Linux']
+];
+$array3 = [4, 5, 6];
 
-var_dump($imePrezime);
+$array[] = 4;
+$array2['gender'] = 'm';
+
+$originalArray = $array;
+krsort($array);
+
+['name' => $name, 'age' => $age, 'skills' => $skills] = $array2;
+
+var_dump(
+    $array, 
+    $originalArray,
+    array_keys($array2),
+    array_values($array2),
+    count($array),
+    array_merge($originalArray, $array3),
+    $name,
+    $age,
+    $skills
+);
