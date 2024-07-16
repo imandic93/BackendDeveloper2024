38e260a26bf80bbab73b9dc6d5543b68f4c2c5f2
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-16 20:26:21 +0200

    Petlje

--- index.php
+++ index.php
@@ -1,44 +1,38 @@
 <?php
 
-$food = 'orange';
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-$return_value = match ($food) {
-    'apple' => 'This food is an apple',
-    'bar' => 'This food is a bar',
-    'cake' => 'This food is a cake',
-    default => 'Unknown food'
-};
-
-var_dump($return_value);
-
-
-$age = 20;
-
-switch ($age) {
-    case 18:
-        echo "Punoljetni ste! Smijete pregledavati naše proizvode!" . PHP_EOL;
-        break;
-
-    case 17:
-        echo 'Vratite se za godinu dana!' . PHP_EOL;
-        break;
-
-    case 16:
-    case 15:
-        echo 'Vratite za par godina!' . PHP_EOL;
-        break;
-    
-    default:
-        echo "Nemate dovoljno godina za prisupiti nasem webu!" . PHP_EOL;
-        break;
+foreach ($numbers as &$number) {
+    $number = $number * $number;
+    // echo $number . PHP_EOL;
 }
 
-if ($age >= 18) {
-    echo "Punoljetni ste! Smijete pregledavati naše proizvode!" . PHP_EOL;
-} elseif ($age === 17) {
-    echo 'Vratite se za godinu dana!' . PHP_EOL;
-} elseif ($age >= 15) {
-    echo 'Vratite za par godina!' . PHP_EOL;
-} else {
-    echo "Nemate dovoljno godina za prisupiti nasem webu!" . PHP_EOL;
-}
+var_dump($numbers);
+
+// $i = 0;
+// do {
+//     echo $numbers[$i] . PHP_EOL;
+//     $i++;
+// } while ($i < count($numbers));
+
+// $i = 0;
+// while ($i < count($numbers)) {
+//     echo $numbers[$i] . PHP_EOL;
+//     $i++;
+// }
+
+// for ($i = 0; $i < count($numbers); $i++) {
+//     echo $numbers[$i] . PHP_EOL;
+// }
+
+// for ($a = 1; $a <= 10; print $a++ . PHP_EOL) {}
+
+// $a = 1;
+
+// while ($a <= 10) {
+//     echo $a++ . PHP_EOL;
+// }
+
+// do {
+//     echo $a++ . PHP_EOL;
+// } while ($a <= 10);
