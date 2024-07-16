89bebf865c65dec2014f4c9e16711ee500bbadfc
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-16 21:07:38 +0200

    Petlje vjezbe

---
+++ vjezbe/petlje_2.php
@@ -0,0 +1,9 @@
+<?php
+
+// Definiranje varijable
+$names = ['Ivan', 'Marko', 'Ana', 'Branimir', 'Marija'];
+
+// Ispis ključeva i vrijednosti niza
+foreach($names as $key => $value) {
+    echo $key . '=' . $value . PHP_EOL;
+}
