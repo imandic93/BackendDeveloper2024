1d557b5cbe199cd4a3f3c60792dc71e9cd4f6c79
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-17 19:29:39 +0200

    Argumenti

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
 <?php
 
-for ($i=2; $i <= 4 ; $i++) { 
-    printSum(getSum(100 * $i, 200));
+function makeyogurt(
+    $container = "bowl", 
+    $flavour = "raspberry", 
+    $style = "Greek",
+)
+{
+    return "Making a $container of $flavour $style yogurt.\n";
 }
 
-function getSum(int $a, int $b)
-{
-    return $a + $b;
-}
+echo makeyogurt(flavour: 'apple');
 
-function printSum(int $sum)
-{
-    echo $sum . PHP_EOL;
-}
+
+// function makecoffee($type = "cappuccino")
+// {
+//     return "Making a cup of $type.\n";
+// }
+// echo makecoffee() . PHP_EOL;
+// echo makecoffee(null) . PHP_EOL;
+// echo makecoffee("espresso") . PHP_EOL;
+
+// function getSum(&$sum, ...$numbers)
+// {
+//     foreach ($numbers as $number) {
+//         $sum += $number;
+//     }
+// }
+
+// function printSum(int $sum)
+// {
+//     echo $sum . PHP_EOL;
+// }
+
+// $sum = 0;
+
+// getSum($sum, 100, 200, 300, 400, 500);
+// getSum($sum, 100, 10000);
+
+// printSum($sum);
