12cbfb957b8b3651a94fc280451ea224895afc33
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-17 20:04:41 +0200

    Argumenti vjezba

---
+++ vjezbe/argumenti.php
@@ -0,0 +1,11 @@
+<?php
+
+// Deklariranje funkcije
+function fullName($name, $surname){
+    $fullName = $name . ' ' . $surname;
+    return strtoupper($fullName);
+}
+// Dodjeljivanje vrijednosti iz funkcije varijabli
+$fullName = fullName('Ivan', 'Horvat');
+// Ispis vrijednosti varijable
+echo $fullName . PHP_EOL;
