0c065d22a0e970a73eb689536f1d792dea0cd9d4
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-17 19:02:35 +0200

    Funkcije vjezbe

---
+++ vjezbe/funkcije.php
@@ -0,0 +1,9 @@
+<?php
+// Deklariranje funkcije
+function returnText(){
+    return 'Vraćeni tekst iz funkcije';
+}
+// Dodjeljivanje vrijednosti iz funkcije varijabli
+$funcText = returnText();
+// Ispis vrijednosti varijable
+echo $funcText . PHP_EOL;
