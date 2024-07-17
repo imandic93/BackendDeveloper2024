845141268b713240e3e4a5de859626ef76fa91a9
Ivan Mandic <ivan.mandic@giscloud.com>
2024-07-17 18:27:22 +0200

    Funkcije

--- index.php
+++ index.php
@@ -1,38 +1,15 @@
 <?php
 
-$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-foreach ($numbers as &$number) {
-    $number = $number * $number;
-    // echo $number . PHP_EOL;
+for ($i=2; $i <= 4 ; $i++) { 
+    printSum(getSum(100 * $i, 200));
 }
 
-var_dump($numbers);
+function getSum(int $a, int $b)
+{
+    return $a + $b;
+}
 
-// $i = 0;
-// do {
-//     echo $numbers[$i] . PHP_EOL;
-//     $i++;
-// } while ($i < count($numbers));
-
-// $i = 0;
-// while ($i < count($numbers)) {
-//     echo $numbers[$i] . PHP_EOL;
-//     $i++;
-// }
-
-// for ($i = 0; $i < count($numbers); $i++) {
-//     echo $numbers[$i] . PHP_EOL;
-// }
-
-// for ($a = 1; $a <= 10; print $a++ . PHP_EOL) {}
-
-// $a = 1;
-
-// while ($a <= 10) {
-//     echo $a++ . PHP_EOL;
-// }
-
-// do {
-//     echo $a++ . PHP_EOL;
-// } while ($a <= 10);
+function printSum(int $sum)
+{
+    echo $sum . PHP_EOL;
+}
