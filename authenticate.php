e9e092b3bd23f9537a6f68183ec4aa7c05f7abd4
Ivan Mandic <ivan.mandic@giscloud.com>
2024-09-02 20:03:29 +0200

    GET i POST

---
+++ authenticate.php
@@ -0,0 +1,10 @@
+<?php
+
+// if (empty($_POST)) {
+//     echo "Form not submitted!";
+//     die();
+// }
+
+echo "Your username is: {$_GET['username']} <br>";
+echo "Your password is: {$_GET['password']} <br>";
+echo "Is admin: {$_GET['admin']} <br>";
--- index.php
+++ index.php
@@ -1,20 +1,14 @@
-<?php
-
-$booksJson = file_get_contents('knjige.json');
-
-$books = json_decode($booksJson, true);
-
-$books[] = [
-    'title' => 'One Hundred Years of Solitude',
-    'author' => 'Gabriel Garcia Marquez',
-    'pages' =>  457,
-    'available' =>  true
-];
-
-$booksJson = json_encode($books);
-
-if (!file_put_contents('knjige.json', $booksJson)) {
-    echo 'Failed storing JSON to file' . PHP_EOL;
-} else {
-    echo 'Stored JSON to file!' . PHP_EOL;
-}
+<html>
+    <body>
+        <p>Login:</p>
+        <form action="authenticate.php" method="get">
+            <label for="username">Username</label>
+            <input type="text" name="username" />
+            <br><br>
+            <label for="password">Password</label>
+            <input type="password" name="password" />
+            <br><br>
+            <input type="submit" value="Login" />
+        </form>
+    </body>
+</html>
