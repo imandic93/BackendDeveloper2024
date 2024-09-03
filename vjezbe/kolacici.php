295d28e5bd295aa3358b0549054e47fbf7a18ec5
Ivan Mandic <ivan.mandic@giscloud.com>
2024-09-03 20:52:50 +0200

    Vjezbe - sesija i kolacici

---
+++ vjezbe/kolacici.php
@@ -0,0 +1,9 @@
+<?php
+// Kreiranje kolačića
+setcookie('EMAIL','test@test.com', time() + 86400, "/");
+// Ispis podatka i kolačića
+echo $_COOKIE['email'];
+// Brisanje kolačića
+setcookie("EMAIL", "", time() - 3600);
+// Ispis varijable kolačića
+print_r($_COOKIE);
