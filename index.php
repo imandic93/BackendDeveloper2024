e2356ec8ef48e4a66c91f9a6463f733f702adb7a
Ivan Mandic <ivan.mandic@giscloud.com>
2024-10-16 18:29:40 +0200

    Mysqli konekcija

--- index.php
+++ index.php
@@ -1,48 +1,9 @@
 <?php
 
-use App\BulletinBoard;
-use App\Enum\MemberType;
-use App\Factory\MemberFactory;
-use App\Group;
-
 require './vendor/autoload.php';
 
-$memberFactory = new MemberFactory();
+$config = require('./config/database.php');
 
-$teacher = $memberFactory->createMember(MemberType::Teacher, 'Ivan', 'Mandic');
-$student = $memberFactory->createMember(MemberType::Student, 'Marko', 'Maric');
-$student2 = $memberFactory->createMember(MemberType::Student, 'Ana', 'Anic');
-$student3 = $memberFactory->createMember(MemberType::Student, 'Iva', 'Ivic');
+$connection = new mysqli(username: $config['username'], password: $config['password'], database: $config['database']);
 
-$board = BulletinBoard::getInstance();
-$board->attach($teacher);
-$board->attach($student3);
-
-$board->addNotification('Za grupu ABC je nastava otkazana iduci tjedan');
-
-$board->attach($student);
-$board->detach($student3);
-$board->addNotification('Grupa 123 pocinje od 17:00 11.10.2024.');
-
-$board->attach($student2);
-
-$group = new Group();
-
-try {
-    $group->addMember($teacher);
-    $group->addMember($student);
-    $group->addMember($student2);
-    $group->addMember($student3);
-    $group->addMember($memberFactory->createMember());
-    $group->addMember($memberFactory->createMember(MemberType::DiscordBot));
-} catch (Exception) {
-    echo "Previse ljudi se pokusalo spojiti!", "\n";
-}
-
-// foreach ($group as $member) {
-//     echo $member->displayJoinedMessage(), "\n";
-// }
-
-// echo "\n";
-
-// $group->displayInfo();
+echo $connection->host_info, "\n";
