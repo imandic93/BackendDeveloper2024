2c75d29dcd5eb0de002491e7d0b0a07fa7fda001
Ivan Mandic <ivan.mandic@giscloud.com>
2024-12-16 18:47:38 +0100

    Break Movie show

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -24,6 +24,8 @@ class MovieController extends Controller
     {
         Gate::authorize('show', Movie::class);
 
-        return Movie::findOrFail($id);
+        $movie = Movie::findOrFail($id);
+
+        return $movies;
     }
 }
